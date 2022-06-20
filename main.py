13469200069ac4724fb332ea922dfb5b24faf7f5
deniszhur <denis11zhuravlev@gmail.com>
2022-06-20 06:52:40 +0000

    first

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def time_now():
             "year" : now.year,
             "month" : now.month,
             "day" : now.day,
+            "hour" : now.hour,
             "minute" : now.minute,
             "second" : now.second,
             "microsecond" : now.microsecond
